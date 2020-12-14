ecc78968287b3123da510bc8586b3e7adeef1825
hhdtc <alex_hhdtc@outlook.com>
2020-12-14 15:49:16 -0500

    some comment

--- backend/app/models/Log.py
+++ backend/app/models/Log.py
@@ -1,7 +1,18 @@
 import logging
 import datetime
+# it record all the activities of event
 class Log():
+
+    # initializing the Log cass
     def __init__(self):
         logging.basicConfig(filename='Lineup.log', encoding='utf-8', level=logging.DEBUG)
-    def logcontent(self, s):
+    
+    #record a message into Lineup.log
+    #argument: string that need to be recorded
+    def logContent(self, s):
         logging.info(str(datetime.datetime.now())+": "+s)
+    
+    #view the Log
+    def viewLog(self):
+        #place holder
+        return False
