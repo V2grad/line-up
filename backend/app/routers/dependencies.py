2504b792bbfe2e10d09686e909897598469303f8
NeverBehave <gayhub@never.pet>
2020-12-16 22:26:32 -0500

    feat: clean up model and docs for schemes

--- backend/app/routers/dependencies.py
+++ backend/app/routers/dependencies.py
@@ -1,10 +1,10 @@
 from fastapi import Header, HTTPException
 
+'''
+    We will retrieve user info here
+'''
+
+
 async def get_token_header(x_token: str = Header(...)):
-    if x_token != "fake-super-secret-token":
+    if x_token != "user-token":
         raise HTTPException(status_code=400, detail="X-Token header invalid")
-
-
-async def get_query_token(token: str):
-    if token != "jessica":
-        raise HTTPException(status_code=400, detail="No Jessica token provided")
--- backend/app/routers/v1/main.py
+++ backend/app/routers/v1/main.py
@@ -4,6 +4,6 @@ from app.routers.v1.endpoints import home, login, users, events
 
 api_router = APIRouter()
 api_router.include_router(home.router, tags=['home'])
-# api_router.include_router(login.router, tags=["login"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
-# api_router.include_router(events.router, prefix="/events", tags=["events"])
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(events.router, prefix="/events", tags=["events"])
